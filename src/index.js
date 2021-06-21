5dfd06a46992cde32b200a8f03bc375a8fca488b
megfly <mflynn333@gmail.com>
2021-06-21 17:15:46 -0400

    initial redux store setup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
+// import reducers
+import rootReducer from './reducers'
+
+//setup redux store
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  // provider gives access to global state, and access to the store, and access to things like dispatch method & get state
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
