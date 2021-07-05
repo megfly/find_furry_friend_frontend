c9ba45425612ebcf70267d0e2e92a2133c46a6a7
megfly <mflynn333@gmail.com>
2021-07-05 13:58:16 -0400

    created and setup initial container component

---
+++ src/containers/ShelterContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import  { connect }  from 'react-redux'
+
+import React, { Component } from 'react'
+
+class ShelterContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default connect()(ShelterContainer)
