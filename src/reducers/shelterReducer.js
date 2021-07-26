94543bd590a82cee490d1d9f0ab8c8e18f38650c
megfly <mflynn333@gmail.com>
2021-07-26 13:27:08 -0400

    added loading shelters and shelters_loaded case statements

--- src/reducers/shelterReducer.js
+++ src/reducers/shelterReducer.js
@@ -1,5 +1,13 @@
 export default (state = {shelters: [], loading: false}, action) => {
     switch(action.type){
+        case("LOADING_SHELTERS"): 
+            //always return new state..never destructive
+            return {...state, loading: true}
+        case("SHELTERS_LOADED"): 
+            return { ...state, 
+                loading: false,
+                shelters: action.payload
+            }
         default:
             return state 
     }
