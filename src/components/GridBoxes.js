b23b13d88c36287ca3ffc35342b093c0e2a145d3
megfly <mflynn333@gmail.com>
2021-08-09 10:24:40 -0400

    modified gridboxes component to add more boxes

---
+++ src/components/GridBoxes.js
@@ -0,0 +1,22 @@
+import "./Box.css"
+
+const GridBoxes = () => {
+    return(
+        <div className="App">
+            <h1>GridBoxes plz work</h1>
+            <div className="grid">
+                <div className="box box1"></div>
+                <div className="box box2"></div>
+                <div className="box box3"></div>
+                <div className="box box4"></div>
+                <div className="box box5"></div>
+                <div className="box box6"></div>
+                <div className="box box7"></div>
+                <div className="box box8"></div>
+                <div className="box box9"></div>
+            </div>
+        </div>
+    )
+}
+
+export default GridBoxes
