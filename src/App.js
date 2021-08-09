c161d6e13ed87a19d6cdb85b08c69df6492f7721
megfly <mflynn333@gmail.com>
2021-08-09 10:23:02 -0400

    imported and added GridBoxes component to see if the css grid boxes were working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import ShelterContainer from './containers/ShelterContainer';
+import GridBoxes from './components/GridBoxes'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Animal Shelters</h3>
+        {/* <h3>Animal Shelters</h3> */}
+        <GridBoxes />
         <ShelterContainer />
       </header>
     </div>
