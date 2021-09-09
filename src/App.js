9b171edd2fc4c990266c4836d3670c78a7e26e2f
megfly <mflynn333@gmail.com>
2021-09-09 11:24:17 -0400

    added PetContainer to app.js in order to see if its working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import ShelterContainer from './containers/ShelterContainer';
 import GridBoxes from './components/GridBoxes'
+import PetContainer from './containers/PetContainer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <h1>Animal Shelters</h1>
         <ShelterContainer />
+        <PetContainer />
       </header>
     </div>
   );
