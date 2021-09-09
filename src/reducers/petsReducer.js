fd134fee4150bdc007d56cedf29ef18949a38d3e
megfly <mflynn333@gmail.com>
2021-09-09 12:59:09 -0400

    created pets reducer case statements

--- src/reducers/petsReducer.js
+++ src/reducers/petsReducer.js
@@ -1,5 +1,13 @@
 export default (state = {pets: [], loading: false}, action) => {
     switch(action.type){
+        case("LOADING_PETS"):
+            //always return new state..never destructive
+            return {...state, loading: true}
+        case("PETS_LOADED"):
+            return { ...state,
+                loading: false,
+                pets: action.payload
+            }
         default:
             return state 
     }
