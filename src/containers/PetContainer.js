4336d29f6e27646abb016dd74dd273fa06e9b000
megfly <mflynn333@gmail.com>
2021-09-09 14:02:58 -0400

    iterated over props.pets

--- src/containers/PetContainer.js
+++ src/containers/PetContainer.js
@@ -4,20 +4,55 @@ import { getPets } from '../actions/pets'
 
 class PetContainer extends Component {
     render() {
+        const pets = this.props.pets.map((pet, index) => {
+                return <ShelterDisplay
+                    key={index}
+                    shelter_id={pet.shelter_id}
+                    id={pet.id}
+                    name={pet.name}
+                    animal_type={pet.animal_type}
+                    breed={pet.breed}
+                    size={pet.size}
+                    gender={pet.gender}
+                    age={pet.age}
+                    color={pet.color}
+                    image={pet.image}
+                    good_with_children={pet.good_with_children}
+                    good_with_dogs={pet.good_with_dogs}
+                    good_with_cats={pet.good_with_cats}
+                    house_trained={pet.house_trained}
+                    vaccinated={pet.vaccinated}
+                />
+            })
         return (
             <div>
-                
+                {pets} 
             </div>
         )
     }
 }
 
 const mapStateToProps = state => {
-    console.log("pets", state)
+    console.log(state)
     return {
-        pets: state.petsreducer.pets,
-        loading: state.petsreducer.loading
+        pets: state.petsReducer.pets,
+        loading: state.petsReducer.loading
     }
 }
 
 export default connect(mapStateToProps, { getPets })(PetContainer)
+
+// :shelter_id,
+// :name,
+// :animal_type,
+// :breed,
+// :size,
+// :gender,
+// :age,
+// :color,
+// :image,
+// :good_with_children,
+// :good_with_dogs,
+// :good_with_cats,
+// :house_trained,
+// :vaccinated
