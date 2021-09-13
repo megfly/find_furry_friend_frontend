4e139a130a2ae1c03dea8fd25416756bee247e64
megfly <mflynn333@gmail.com>
2021-09-13 10:37:04 -0400

    troubleshooting componentDidMount

--- src/containers/PetContainer.js
+++ src/containers/PetContainer.js
@@ -4,32 +4,39 @@ import { getPets } from '../actions/pets'
 import PetDisplay from '../components/PetDisplay'
 
 class PetContainer extends Component {
+
+    componentDidMount(){
+        debugger
+        this.props.getPets()
+    }
+
     render() {
-        const pets = this.props.pets.map((pet, index) => {
-                return <PetDisplay
-                    key={index}
-                    shelter_id={pet.shelter_id}
-                    id={pet.id}
-                    name={pet.name}
-                    animal_type={pet.animal_type}
-                    breed={pet.breed}
-                    size={pet.size}
-                    gender={pet.gender}
-                    age={pet.age}
-                    color={pet.color}
-                    image={pet.image}
-                    good_with_children={pet.good_with_children}
-                    good_with_dogs={pet.good_with_dogs}
-                    good_with_cats={pet.good_with_cats}
-                    house_trained={pet.house_trained}
-                    vaccinated={pet.vaccinated}
-                />
-            })
-        return (
-            <div>
-                {pets} 
-            </div>
-        )
+        return <div></div>
+        // const pets = this.props.pets.map((pet, index) => {
+        //         return <PetDisplay
+        //             key={index}
+        //             shelter_id={pet.shelter_id}
+        //             id={pet.id}
+        //             name={pet.name}
+        //             animal_type={pet.animal_type}
+        //             breed={pet.breed}
+        //             size={pet.size}
+        //             gender={pet.gender}
+        //             age={pet.age}
+        //             color={pet.color}
+        //             image={pet.image}
+        //             good_with_children={pet.good_with_children}
+        //             good_with_dogs={pet.good_with_dogs}
+        //             good_with_cats={pet.good_with_cats}
+        //             house_trained={pet.house_trained}
+        //             vaccinated={pet.vaccinated}
+        //         />
+        //     })
+        // return (
+        //     <div>
+        //         {pets} 
+        //     </div>
+        // )
     }
 }
 
@@ -42,7 +49,8 @@ const mapStateToProps = state => {
 }
 //error here????
 //export default connect(mapStateToProps, { getPets })(PetContainer)
-export default connect(mapStateToProps)(PetContainer)
+//export default connect(mapStateToProps)(PetContainer)
+export default PetContainer
 
 // :shelter_id,
 // :name,
