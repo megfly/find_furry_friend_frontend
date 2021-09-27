e0600f13e57d716d7258623901befc0f71275379
megfly <mflynn333@gmail.com>
2021-09-27 10:10:23 -0400

    created search input, handleSearch function, filterPets function and passed down to PetDisplay- data currently not displaying

--- src/containers/PetContainer.js
+++ src/containers/PetContainer.js
@@ -5,72 +5,75 @@ import { getPets } from '../actions/pets'
 import PetDisplay from '../components/PetDisplay'
 
 class PetContainer extends Component {
+    //local state
+    state = {
+        searchinput: ''
+    }
 
     componentDidMount(){ 
-        //this is working, when this is commented out- the pets arent in state
         this.props.getPets()
     }
 
-    //MY PETS ARE IN MY STATE BUT THEY ARENT GOING IN MY PROPS
+    handleSearch = (event) => {
+        // console.log("search", event.target.value)
+        this.setState({
+            searchinput: event.target.value
+        })
+    }
+
+    filterPets = () => {
+        if (this.state.searchinput !== '') {
+            return this.props.pets.filter(pet => {
+                // debugger 
+                return pet.breed.includes(this.state.searchinput)
+            })
+        } else {
+            return this.props.pets
+        }
+    }
 
     render() {
         console.log("pets props", this.props)
-                // const pets = this.props.pets.map((pet, index) => {
-        //         return <PetDisplay
-        //             key={index}
-        //             shelter_id={pet.shelter_id}
-        //             id={pet.id}
-        //             name={pet.name}
-        //             animal_type={pet.animal_type}
-        //             breed={pet.breed}
-        //             size={pet.size}
-        //             gender={pet.gender}
-        //             age={pet.age}
-        //             color={pet.color}
-        //             image={pet.image}
-        //             good_with_children={pet.good_with_children}
-        //             good_with_dogs={pet.good_with_dogs}
-        //             good_with_cats={pet.good_with_cats}
-        //             house_trained={pet.house_trained}
-        //             vaccinated={pet.vaccinated}
-        //         />
-        //     }
-        // )
-        return (
-            <div>
-                
-            </div>
-        )
 
-        // const pets = this.props.pets.map((pet, index) => {
-        //         return <PetDisplay
-        //             key={index}
-        //             shelter_id={pet.shelter_id}
-        //             id={pet.id}
-        //             name={pet.name}
-        //             animal_type={pet.animal_type}
-        //             breed={pet.breed}
-        //             size={pet.size}
-        //             gender={pet.gender}
-        //             age={pet.age}
-        //             color={pet.color}
-        //             image={pet.image}
-        //             good_with_children={pet.good_with_children}
-        //             good_with_dogs={pet.good_with_dogs}
-        //             good_with_cats={pet.good_with_cats}
-        //             house_trained={pet.house_trained}
-        //             vaccinated={pet.vaccinated}
-        //         />
-        //     }
-        // )
-        // return (
-        //     <div>
-        //         {pets} 
-        //     </div>
-        // )
+        return (
+            <Fragment>
+                <br />
+                <input 
+                    type="text"
+                    name="pet-search-by-breed"
+                    placeholder="search by breed..."
+                    onChange={this.handleSearch}
+                    value={this.state.searchinput}
+                />
+                <br />
+                <div className="grid">
+                    {this.props.loading ? <h3>Loading...</h3> : this.filterPets().map((pet, index) => {
+                    <PetDisplay
+                        key={index}
+                        shelter_id={pet.shelter_id}
+                        id={pet.id}
+                        name={pet.name}
+                        animal_type={pet.animal_type}
+                        breed={pet.breed}
+                        size={pet.size}
+                        gender={pet.gender}
+                        age={pet.age}
+                        color={pet.color}
+                        image={pet.image}
+                        good_with_children={pet.good_with_children}
+                        good_with_dogs={pet.good_with_dogs}
+                        good_with_cats={pet.good_with_cats}
+                        house_trained={pet.house_trained}
+                        vaccinated={pet.vaccinated}
+                />
+                    }) }
+                   
+                </div>
+            </Fragment>
+            ) 
+        }
     }
-}
-        
+
 
 //structure what your props look like
 const mapStateToProps = state => {
