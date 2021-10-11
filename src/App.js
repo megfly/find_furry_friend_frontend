a24369cfa3e8512b9cffb70e4f8759e87684869a
megfly <mflynn333@gmail.com>
2021-10-11 10:13:54 -0400

    created content for home file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import ShelterContainer from './containers/ShelterContainer';
-import PetContainer from './containers/PetContainer';
+import Router from './components/Router'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Animal Shelters</h1>
-        <ShelterContainer />
-        <h1>Pets</h1>
-        <PetContainer />
+        <Router />
       </header>
     </div>
   );
