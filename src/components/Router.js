c50f62d067c5bcb77bffe4e4f599fab829e6724f
megfly <mflynn333@gmail.com>
2021-10-11 09:46:10 -0400

    imported components

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Home from '../components/Home'
+import PetContainer from '../containers/PetContainer'
+import ShelterContainer from '../containers/ShelterContainer'
 
 export default function Router() {
     return (
