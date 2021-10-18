eccfb5b4faf4a4ec938b720014967de5d5a2159d
megfly <mflynn333@gmail.com>
2021-10-18 09:11:03 -0400

    imported react router Link and added appropriate text to display

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Link
+  } from "react-router-dom";
 
 export default function Home() {
     return (
         <div>
-            This is a Home Page
+            Animal Shelters
+            <br />
+            Pets 
         </div>
     )
 }
